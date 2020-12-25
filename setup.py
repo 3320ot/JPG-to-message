7f3da6c7345491afbd0015cd1db6750763618c7b
3320ot <73736364+3320ot@users.noreply.github.com>
2020-12-25 22:34:31 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import converter
+
+setup(
+    name='JPG-into-message-main',
+    version=converter.__version__,
+    packages=find_packages(),
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts':
+            ['converter = converter.Converter:converter']
+        }
+    
+    )
