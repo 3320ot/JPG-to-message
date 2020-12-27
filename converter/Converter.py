49cf2c62902e40124deb9154034aa5a6595a8f91
3320ot <73736364+3320ot@users.noreply.github.com>
2020-12-28 01:10:58 +1000

    Update Converter.py

--- converter/Converter.py
+++ converter/Converter.py
@@ -33,13 +33,13 @@ def converter():
         elif colors >= 11 and colors <= 50:
             b[i]  = '4'
         elif colors >= 51 and colors <= 70:
-            b[i]  = '3'
+            b[i]  = '5'
         elif colors >= 101 and colors <= 130:
             b[i]  = '3'
         elif colors >= 131 and colors <= 150:
             b[i]  = '9'
         elif colors >= 151 and colors <= 200:
-            b[i]  = '2'
+            b[i]  = '3'
         elif colors >= 71 and colors <= 100:
             b[i]  = '2'
         elif colors >= 201 and colors <= 255:
